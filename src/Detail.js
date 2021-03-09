4aabb5f2d73850edb3fca154eabb30b6bf67c364
Anıl <trogonya@gmail.com>
2021-03-09 21:56:11 +0300

    first commit

---
+++ src/Detail.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+
+
+export default function Detail() {
+
+  return (
+    <div className="body">
+      <Grid className="main-container">
+        sads
+      </Grid>
+    </div>
+  );
+}
+
